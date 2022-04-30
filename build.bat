2bbe7e7d0d4f1bb1cef28fbd8a6dfa0c110ac1d3
hohfchns <hohfchns@gmail.com>
2022-04-30 20:38:46 +0300

    Fixed the build process for all systems (for the most part)
    In order to get it to consistently compile among multiple systems I had to also include the i686 libraries in the "libdirs" variable, which on my machine when included still compiles, but puts out a bunch of warnings.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 del .\HOEngine.exe
 
 .\vendor\premake5\premake5.exe gmake2
-mingw32-make RAYLIB_BUILD_ARCH=x64 -j8 config="%~1"
+mingw32-make -j8 config="%~1"
 
 if "%~2" == "run" ( .\HOEngine.exe )
