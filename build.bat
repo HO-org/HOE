5ea6417cbe1c9d8ccc2c3d9805078196041a704e
hohfchns <hohfchns@gmail.com>
2022-04-30 12:42:26 +0300

    Updated the build process to remove unnecessary step

--- build.bat
+++ build.bat
@@ -1,9 +1,6 @@
-del .\build\Debug\HOEngine.exe
 del .\HOEngine.exe
 
 premake5 gmake2
 mingw32-make RAYLIB_BUILD_ARCH=x64 -j8 config="%~1"
-
-copy .\build\"%~1"\HOEngine.exe .\
 
 if "%~2" == "run" ( .\HOEngine.exe )
--- premake5.lua
+++ premake5.lua
@@ -4,7 +4,7 @@ workspace "HOEngine"
 project "HOEngine"
     kind "ConsoleApp"
     language "C++"
-    targetdir "build/%{cfg.buildcfg}"
+    targetdir "."
 
     includedirs { "include", "vendor/SDL2-2.0.20/x86_64-w64-mingw32/include/SDL2", "vendor/SDL2_image-2.0.5/x86_64-w64-mingw32/include/SDL2" }
     includedirs { "src/**" }
