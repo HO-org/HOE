4d2472a95dc7d0e71b7424cdc1899574d1d19dfc
hohfchns <hohfchns@gmail.com>
2022-05-02 10:19:10 +0300

    Project now builds on Linux!

--- premake5.lua
+++ premake5.lua
@@ -1,20 +1,35 @@
 workspace "HOEngine"
     configurations { "Debug", "Release" }
+    architecture "x86_64"
 
 project "HOEngine"
     kind "ConsoleApp"
     language "C++"
     targetdir "."
-
-    includedirs { "include", "vendor/SDL2-2.0.20/x86_64-w64-mingw32/include/SDL2", "vendor/SDL2_image-2.0.5/x86_64-w64-mingw32/include/SDL2" }
-    includedirs { "src/**" }
-    
+	
     -- libdirs { "lib", "../lib", "vendor/SDL2-2.0.20/x86_64-w64-mingw32/lib", "vendor/SDL2_image-2.0.5/x86_64-w64-mingw32/lib" }
-    libdirs { "vendor/SDL2-2.0.20/x86_64-w64-mingw32/**", "vendor/SDL2_image-2.0.5/x86_64-w64-mingw32/**" }
     -- libdirs { "vendor/**" }
-
-    links { "mingw32", "SDL2main", "SDL2", "SDL2_image" }
     
+	filter "system:windows"
+	    includedirs { "include", "vendor/SDL2-2.0.20/x86_64-w64-mingw32/include/SDL2", "vendor/SDL2_image-2.0.5/x86_64-w64-mingw32/include/SDL2" }
+	    
+	    libdirs { "vendor/SDL2-2.0.20/x86_64-w64-mingw32/**", "vendor/SDL2_image-2.0.5/x86_64-w64-mingw32/**" }
+    	links { "mingw32", "SDL2main", "SDL2", "SDL2_image" }
+    	
+    	targetname "HOEngine.exe"
+	
+	filter "system:linux"
+		includedirs { "/usr/include/SDL2" }
+		
+		libdirs { os.findlib("SDL2"), os.findlib("SDL2_image") }
+		removelibdirs { "/**/lib32" } 
+    	links { "SDL2", "SDL2_image" }
+    	
+    	targetname "HOEngine.game"
+--		includedirs { "include/SDL2-linux" }
+	
+    includedirs { "src/**" }
+	
     files { "src/**.h", "src/**.cpp" }
 
     removefiles { "deprecated/**.h", "deprecated/**.cpp", "vendor/loguru-2.1.0/**" }
